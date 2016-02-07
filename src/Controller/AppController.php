e6992ee40beccf0e277287622750e815c3ab5ad4
Jani Mäkelä <jani.makela@pp3.inet.fi>
2016-02-07 16:54:35 +0000

    Disallowed everything except adding users and login from quests.

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -70,7 +70,7 @@ class AppController extends Controller
      */
     public function beforeFilter(Event $event)
     {
-        $this->Auth->allow(['index', 'view', 'display']);
+        //$this->Auth->allow(['index', 'view', 'display']);
     }
 
     public function isAuthorized($user)
