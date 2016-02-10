12787d3f82d729efe6f4c7aa25d89e568a6a1619
Jani Mäkelä <jani.makela@pp3.inet.fi>
2016-02-10 01:42:24 +0000

    Made a 'default' action bar

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -93,6 +93,8 @@ class AppController extends Controller
      */
     public function beforeRender(Event $event)
     {
+        $this->set('isAdmin', $this->Auth->user('role') === 'Admin');
+
         if (!array_key_exists('_serialize', $this->viewVars) &&
             in_array($this->response->type(), ['application/json', 'application/xml'])
         ) {
